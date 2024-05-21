f9e78e87652977315ec9b8fe1cb692e2204f4ab1
MFG38 <santtu.pesonen@mail.com>
2024-05-21 11:41:15 +0300

    Beginnings of the date parser

--- src/dateParser.py
+++ src/dateParser.py
@@ -8,3 +8,46 @@
 '''
 
 import datetime
+import calendar
+
+calendar.setfirstweekday(calendar.MONDAY)
+
+def parse_weekday_from_string(weekday: str):
+    parsed_wd = 0
+
+    match weekday.lower():
+        case "today":
+            parsed_wd = datetime.weekday()
+        case "tomorrow":
+            parsed_wd = (datetime.weekday() + 1) % 7
+        case "mon" | "monday":
+            parsed_wd = 0
+        case "tue" | "tuesday":
+            parsed_wd = 1
+        case "wed" | "wednesday":
+            parsed_wd = 2
+        case "thu" | "thursday":
+            parsed_wd = 3
+        case "fri" | "friday":
+            parsed_wd = 4
+        case "sat" | "saturday":
+            parsed_wd = 5
+        case "sun" | "sunday":
+            parsed_wd = 6
+        case _:
+            pass
+
+    return parsed_wd
+
+def parse_deadline_from_string(deadline: str):
+    pass
+
+def convert_string_to_date(deadline: str):
+    pass
+
+if __name__ == "__main__":
+    print(datetime.date.today())
+
+    while True:
+        p = parse_weekday_from_string(input())
+        print(p)
