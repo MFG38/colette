17a15db24a66ee931cd6d402349daa4dbc6dec0f
MFG38 <santtu.pesonen@mail.com>
2024-06-10 12:01:28 +0300

    Print reminders for tasks due tomorrow

--- src/reminder.py
+++ src/reminder.py
@@ -5,6 +5,8 @@
     approaching due dates.
 '''
 
+from datetime import timedelta
+
 import todoHandler as th
 import dateParser as dtp
 from textColors import *
@@ -14,17 +16,31 @@ from dueDateUpdater import *
 
 def print_reminders():
     tasks_due_today = []
+    tasks_due_tomorrow = []
     tasks_with_passed_deadlines = []
 
     for entry in th.todo:
         if entry.deadline == dtp.get_current_full_date():
             tasks_due_today.append(entry.desc)
+        elif entry.deadline == (dtp.get_current_full_date() + timedelta(days=1)):
+            tasks_due_tomorrow.append(entry.desc)
         elif entry.deadline < dtp.get_current_full_date():
             tasks_with_passed_deadlines.append(entry.desc)
 
-    if len(tasks_due_today) > 0:
+    if len(tasks_due_today) > 0 and len(tasks_due_tomorrow) == 0:
         print(f"{TextColor.REMINDER}REMINDER: You have {len(tasks_due_today)} task(s) due today:{TextColor.RESET}")
         for i in tasks_due_today:
+            print(i, sep=", ")
+        print()
+    elif len(tasks_due_tomorrow) > 0 and len(tasks_due_today) == 0:
+        print(f"{TextColor.REMINDER}REMINDER: You have {len(tasks_due_tomorrow)} task(s) due tomorrow:{TextColor.RESET}")
+        for i in tasks_due_tomorrow:
+            print(i, sep=", ")
+        print()
+    elif len(tasks_due_today) > 0 and len(tasks_due_tomorrow) > 0:
+        tasks_due_soon = list(tasks_due_today + tasks_due_tomorrow)
+        print(f"{TextColor.REMINDER}REMINDER: You have {len(tasks_due_today)} task(s) due today and {len(tasks_due_tomorrow)} due tomorrow:{TextColor.RESET}")
+        for i in tasks_due_soon:
             print(i, sep=", ")
         print()
 
